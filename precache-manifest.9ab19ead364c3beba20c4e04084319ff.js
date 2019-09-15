02039380e07945de6704947c2d756225ea166da1
YardenShoham <hrsi88@gmail.com>
2019-09-15 07:43:40 +0300

    Updates

---
+++ precache-manifest.9ab19ead364c3beba20c4e04084319ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be36e422fcca49e93e92c5e1f8699b68",
+    "url": "/lang-switch/index.html"
+  },
+  {
+    "revision": "54dfc1f04aa4823d13cd",
+    "url": "/lang-switch/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "cd9029123736ee1b353e",
+    "url": "/lang-switch/static/js/2.8c7389c1.chunk.js"
+  },
+  {
+    "revision": "54dfc1f04aa4823d13cd",
+    "url": "/lang-switch/static/js/main.302b27a1.chunk.js"
+  },
+  {
+    "revision": "559b057ff7cdacde9147",
+    "url": "/lang-switch/static/js/runtime~main.e94ba745.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lang-switch/static/media/logo.5d5d9eef.svg"
+  }
+]);

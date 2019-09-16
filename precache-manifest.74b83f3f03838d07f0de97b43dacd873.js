efa912b3daa83b7711c49bd5f4f1b4b894cae786
YardenShoham <hrsi88@gmail.com>
2019-09-16 17:24:37 +0300

    Updates

--- precache-manifest.74b83f3f03838d07f0de97b43dacd873.js
+++ precache-manifest.74b83f3f03838d07f0de97b43dacd873.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dc29edd426c5ad6ce8a5b09f5adf4993",
+    "revision": "e6c1eb7a15b1743493b0a2a0747c208b",
     "url": "/lang-switch/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/lang-switch/static/css/2.edc2e41f.chunk.css"
   },
   {
-    "revision": "26fdca088f9003c1e3c1",
+    "revision": "b4c00f3b91c33a8af1b1",
     "url": "/lang-switch/static/css/main.3c5a145d.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/lang-switch/static/js/2.cfdf00d7.chunk.js"
   },
   {
-    "revision": "26fdca088f9003c1e3c1",
-    "url": "/lang-switch/static/js/main.d5205d62.chunk.js"
+    "revision": "b4c00f3b91c33a8af1b1",
+    "url": "/lang-switch/static/js/main.1cfa3649.chunk.js"
   },
   {
     "revision": "559b057ff7cdacde9147",

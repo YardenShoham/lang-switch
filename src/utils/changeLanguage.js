98ed847c8ed8235154a45859ef73893fe0c64681
YardenShoham <hrsi88@gmail.com>
2019-09-16 20:03:48 +0300

    add docs

--- src/utils/changeLanguage.js
+++ src/utils/changeLanguage.js
@@ -1,10 +1,20 @@
-const languages = require("../resources/languages");
+const languages = require("../resources/languages"); // get languages
 
+/**
+ * Class with the ability to "change" one language to another.
+ */
 class Changer {
     constructor() {
         this.languages = languages;
     }
 
+    /**
+     * Creates a Map from <code>fromLanguage</code> to <code>toLanguage</code>.
+     *
+     * This map will be used for every change() call.
+     * @param {String} fromLanguage The language to "change" from.
+     * @param {String} toLanguage The destination language.
+     */
     init(fromLanguage, toLanguage) {
         this.languageMap = new Map();
         for (let i = 0; i < 40; i++) {
@@ -15,6 +25,12 @@ class Changer {
         }
     }
 
+    /**
+     * Changes a given text according to the Map set with init().
+     *
+     * @param {String} inputText String to be "changed".
+     * @returns {String} "Changed" text.
+     */
     change(inputText) {
         const lowerInputText = inputText.toLowerCase();
         let outputText = "";

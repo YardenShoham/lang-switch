101dbbc43bf77a7386ae46809a60488085340dfd
YardenShoham <hrsi88@gmail.com>
2019-09-20 23:20:07 +0300

    add CopyPasteButtons.jsx docs

--- src/components/CopyPasteButtons.jsx
+++ src/components/CopyPasteButtons.jsx
@@ -2,6 +2,16 @@ import React from "react";
 import CopyButton from "./common/CopyButton";
 import PasteButton from "./common/PasteButton";
 
+/**
+ * A component that has both a Copy and Paste buttons.
+ *
+ * General Look:
+ * <<Paste Button>>           <<Copy Button>>
+ *
+ * @param {function} onCopy The function that will be called when the Copy button is clicked.
+ * @param {function} onPaste The function that will be called when the Paste button is clicked.
+ * @returns {ReactComponent} The set of buttons.
+ */
 const CopyPasteButtons = ({ onCopy, onPaste }) => {
     return (
         <div className="form-group">

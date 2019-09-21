ecd2c157a384bb71c7bdc72101d957e18aa04f67
YardenShoham <hrsi88@gmail.com>
2019-09-21 10:15:32 +0300

    remove duplicated code

--- src/__tests__/components/common/SwapButton.test.js
+++ src/__tests__/components/common/SwapButton.test.js
@@ -2,7 +2,6 @@ import "@testing-library/jest-dom/extend-expect";
 import React from "react";
 import { render } from "@testing-library/react";
 import SwapButton from "./../../../components/common/SwapButton";
-import { notDeepEqual } from "assert";
 
 describe("<SwapButton />", () => {
     it("should have a button with the btn, btn-primary, btn-lg and btn-block classes (exactly these 4) by default", () => {
@@ -16,14 +15,6 @@ describe("<SwapButton />", () => {
             "btn-block"
         );
         expect(Object.keys(innerButton.classList).length).toBe(4);
-    });
-
-    it("should have a button with additional classes when provided", () => {
-        const classes = "class1 class2 class3";
-        const { container } = render(<SwapButton className={classes} />);
-        const innerButton = container.querySelector("button");
-
-        expect(innerButton).toHaveClass(...classes.split(/ /));
     });
 
     it('should have an alt text showing the word "Swap"', () => {
--- src/components/common/SwapButton.jsx
+++ src/components/common/SwapButton.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { faExchangeAlt } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Button from "./Button";
 
 /**
